d36cfe7d6f6bee59f30a50e3f3ae872ca7aa4fbc
Jeff Squires <jeff.squires@gmail.com>
2012-05-17 16:31:15 -0400

    unscrews up alias_method_chain in delete

--- app/models/address_decorator.rb
+++ app/models/address_decorator.rb
@@ -18,14 +18,16 @@ Spree::Address.class_eval do
   def to_s
     "#{firstname} #{lastname}: #{zipcode}, #{country}, #{state || state_name}, #{city}, #{address1} #{address2}"
   end
-  
-  def destroy_with_saving_used
+
+  # stole this problematic alias_method_chain from https://github.com/iloveitaly/spree_address_book/blob/af7ceb22ddf9c5c8d63dfbcac7bf8440dffbea1a/app/models/spree/address_decorator.rb
+
+  # as of version 1.1 Spree::Address does not have a custom destroy method
+  # if in the future it is added, this may cause issues
+  def destroy
     if can_be_deleted?
-      destroy_without_saving_used
+      super
     else
       update_attribute(:deleted_at, Time.now)
     end
   end
-  alias_method_chain :destroy, :saving_used
-
 end
