2fd469e8e3e745d567a51c33e808f7617d1add11
Jeff Squires <jeff.squires@gmail.com>
2013-08-07 15:17:52 -0400

    fixes some major issues w/ the use_billing checkbox

--- app/assets/javascripts/store/spree_address_book.js
+++ app/assets/javascripts/store/spree_address_book.js
@@ -1,25 +1,29 @@
 (function($) {
   $(document).ready(function(){
+    $('input#order_use_billing').unbind("click");
+
+    $('input#order_use_billing').click(function() {
+      if ($(this).is(':checked')) {
+        $("#shipping .select_address").hide();
+        hide_address_form('shipping');
+      } else {
+        $("#shipping .select_address").show();
+
+        if ($("input[name='order[ship_address_id]']").length == 0 || $("input[name='order[ship_address_id]']:checked").val() == '0') {
+          show_address_form('shipping');
+        }
+      }
+    });
+
+    if ($('input#order_use_billing').is(':checked')) {
+      $("#shipping .select_address").hide();
+      hide_address_form('shipping');
+    }
+
     if ($(".select_address").length) {
-      $('input#order_use_billing').unbind("click");
       $(".inner").hide();
       $(".inner input").prop("disabled", true);
       $(".inner select").prop("disabled", true);
-      if ($('input#order_use_billing').is(':checked')) {
-        $("#shipping .select_address").hide();
-      }
-      
-      $('input#order_use_billing').click(function() {
-        if ($(this).is(':checked')) {
-          $("#shipping .select_address").hide();
-          hide_address_form('shipping');
-        } else {
-          $("#shipping .select_address").show();
-          if ($("input[name='order[ship_address_id]']:checked").val() == '0') {
-            show_address_form('shipping');
-          }
-        }
-      });
 
       $("input[name='order[bill_address_id]']:radio").change(function(){
         if ($("input[name='order[bill_address_id]']:checked").val() == '0') {
@@ -38,13 +42,13 @@
       });
     }
   });
-  
+
   function hide_address_form(address_type){
     $("#" + address_type + " .inner").hide();
     $("#" + address_type + " .inner input").prop("disabled", true);
     $("#" + address_type + " .inner select").prop("disabled", true);
   }
-  
+
   function show_address_form(address_type){
     $("#" + address_type + " .inner").show();
     $("#" + address_type + " .inner input").prop("disabled", false);
