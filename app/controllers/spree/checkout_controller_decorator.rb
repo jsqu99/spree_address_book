f9a8e8fa62c3650865def866b3de854c7afb3f4a
Jeff Squires <jeff.squires@gmail.com>
2013-11-06 16:27:24 -0500

    do not run after filter if there is a problem with the order

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -22,7 +22,7 @@ Spree::CheckoutController.class_eval do
   end
   
   def normalize_addresses
-    return unless params[:state] == "address" && @order.bill_address_id && @order.ship_address_id
+    return unless params[:state] == "address" && order.errors.empty? && @order.bill_address_id && @order.ship_address_id
     @order.bill_address.reload
     @order.ship_address.reload
     
